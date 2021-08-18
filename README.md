78415c902979c2f3dd8af448b73e9ed682383101
SHLOK2345 <83119967+SHLOK2345@users.noreply.github.com>
2021-08-18 15:46:46 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# c44-remastered
+# p5.play-boilerplate
+Boiler plate for p5.play
---
+++ sketch.js
@@ -0,0 +1,23 @@
+const Engine=Matter.Engine
+const Bodies=Matter.Bodies
+const World=Matter.World
+
+
+
+
+function setup() {
+  createCanvas(displayWidth,displayHeight);
+  
+
+  engine=Engine.create()
+  world=engine.world
+  Engine.run(engine)
+
+  bird1=new Bird()
+}
+
+function draw() {
+  background(0);  
+  drawSprites();
+  bird1.display()
+}
